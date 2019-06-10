33d738ff10a05f2810b2cb4ad3337df942fe036c
Cory O'Daniel <cory@coryodaniel.com>
2019-06-10 16:18:00 -0700

    bump version, missing csinodes create verb

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule K8s.MixProject do
     [
       app: :k8s,
       description: "Kubernetes API Client for Elixir",
-      version: "0.2.8",
+      version: "0.2.9",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
