26884f4e51ee677050231634064e72434c078318
Cory O'Daniel <cory@coryodaniel.com>
2019-07-02 15:53:29 -0700

    add subresource support to K8s.Cluster.Path

--- test/k8s/cluster/path_test.exs
+++ test/k8s/cluster/path_test.exs
@@ -2,11 +2,14 @@ defmodule K8s.Cluster.PathTest do
   use ExUnit.Case, async: true
   doctest K8s.Cluster.Path
 
-  defp resource_definition() do
+  defp deployment_definition() do
     %{
-      "kind" => "Pod",
-      "name" => "pods",
+      "categories" => ["all"],
+      "kind" => "Deployment",
+      "name" => "deployments",
       "namespaced" => true,
+      "shortNames" => ["deploy"],
+      "singularName" => "",
       "verbs" => [
         "create",
         "delete",
@@ -20,10 +23,61 @@ defmodule K8s.Cluster.PathTest do
     }
   end
 
+  def deployment_scale_definition() do
+    %{
+      "group" => "apps",
+      "kind" => "Scale",
+      "name" => "deployments/scale",
+      "namespaced" => true,
+      "singularName" => "",
+      "verbs" => ["get", "patch", "update"],
+      "version" => "v1beta1"
+    }
+  end
+
+  def deployment_status_definition() do
+    %{
+      "kind" => "Deployment",
+      "name" => "deployments/status",
+      "namespaced" => true,
+      "singularName" => "",
+      "verbs" => ["get", "patch", "update"]
+    }
+  end
+
   describe "build/4" do
-    test "when a required param is missing, returns an error tuple" do
-      res = K8s.Cluster.Path.build("v1", resource_definition(), :update, namespace: "default")
-      assert {:error, :missing_required_param, [:name]} = res
+    test "when a required param is missing, returns an error" do
+      error = K8s.Cluster.Path.build("v1", deployment_definition(), :update, namespace: "default")
+      assert {:error, :missing_required_param, [:name]} = error
+    end
+
+    test "given a subresource, when a resource param is missing, returns an error" do
+      error =
+        K8s.Cluster.Path.build("v1", deployment_scale_definition(), :update, namespace: "default")
+
+      assert {:error, :missing_required_param, [:name]} = error
+    end
+
+    test "given a subresource with an alternate kind, returns a path" do
+      # i.e.: {"apps/v1", "Scale", "deployments/scale"}
+      {:ok, path} =
+        K8s.Cluster.Path.build("v1", deployment_scale_definition(), :update,
+          namespace: "default",
+          name: "foo"
+        )
+
+      assert path == "/api/v1/namespaces/default/deployments/foo/scale"
+    end
+
+    test "given a subresource with the same kind, returns a path" do
+      # i.e.: {"apps/v1", "Deployment", "deployments/status"}
+      {:ok, path} =
+        K8s.Cluster.Path.build("v1", deployment_status_definition(), :update,
+          namespace: "default",
+          name: "foo"
+        )
+
+      assert path == "/api/v1/namespaces/default/deployments/foo/status"
     end
   end
 end
