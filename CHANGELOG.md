c8e4cb61d5011a073ee08d8c3ded235d4a5077c4
Cory O'Daniel <cory@coryodaniel.com>
2019-08-15 10:34:43 -0700

    v 0.3.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+## [0.3.1] - 2019-08-15
+
 ### Added
+
 - text/plain response handling
 - K8s.Selector - labelSelector support for K8s.Operation
 
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule K8s.MixProject do
     [
       app: :k8s,
       description: "Kubernetes API Client for Elixir",
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
