15820c5533be3f3327befe474ce9c2cee43e5a5f
Cory O'Daniel <cory@coryodaniel.com>
2019-11-27 13:27:33 -0800

    registry state impl

--- lib/k8s/middleware/registry.ex
+++ lib/k8s/middleware/registry.ex
@@ -1,21 +1,41 @@
 defmodule K8s.Middleware.Registry do
-  @moduledoc """
-  Cluster middleware registry
-  """
+  @moduledoc "Cluster middleware registry"
   use Agent
 
-  @spec start_link(map()) :: :ok
-  def start_link(registry = %{}) do
-    Agent.start_link(fn -> registry end, name: __MODULE__)
+  @spec start_link(Keyword.t()) :: Agent.on_start()
+  def start_link(_opts) do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
   end
 
   @doc "Adds a middleware to the end of the middleware stack"
   @spec add(atom, :request | :response, module()) :: :ok
   def add(cluster, type, middleware) do
+    Agent.update(__MODULE__, fn registry ->
+      cluster_middlewares = Map.get(registry, cluster, %{})
+      middleware_list = Map.get(cluster_middlewares, type, [])
+
+      updated_middleware_list = middleware_list ++ [middleware]
+      updated_cluster_middlewares = Map.put(cluster_middlewares, type, updated_middleware_list)
+
+      put_in(registry, [cluster], updated_cluster_middlewares)
+    end)
   end
 
   @doc "Sets/replaces the middleware stack"
   @spec set(atom, :request | :response, list(module())) :: :ok
   def set(cluster, type, middlewares) do
+    Agent.update(__MODULE__, fn registry ->
+      cluster_middlewares = Map.get(registry, cluster, %{})
+      updated_cluster_middlewares = Map.put(cluster_middlewares, type, middlewares)
+
+      put_in(registry, [cluster], updated_cluster_middlewares)
+    end)
+  end
+
+  @doc "Returns middleware stack for a cluster and (request or response)"
+  @spec list(atom, :request | :response) :: :ok
+  def list(cluster, type) do
+    registry = Agent.get(__MODULE__, & &1[cluster]) || %{}
+    Map.get(registry, type, [])
   end
 end
--- test/k8s/middleware/registry_test.exs
+++ test/k8s/middleware/registry_test.exs
@@ -1,3 +1,57 @@
 defmodule K8s.Middleware.RegistryTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case, async: false
+  alias K8s.Middleware.Registry
+
+  defmodule BarMiddleware do
+    @behaviour K8s.Middleware.Request
+    def call(req), do: {:ok, req}
+  end
+
+  defmodule FooMiddleware do
+    @behaviour K8s.Middleware.Request
+    def call(req), do: {:ok, req}
+  end
+
+  describe "list/2" do
+    test "returns an empty list when none are registered", %{test: id} do
+      assert [] == Registry.list(id, :request)
+    end
+
+    test "returns registered middlewares modules", %{test: id} do
+      mw = K8s.Middleware.RegistryTest.FooMiddleware
+      :ok = Registry.add(id, :request, mw)
+      middlewares = Registry.list(id, :request)
+
+      assert middlewares == [K8s.Middleware.RegistryTest.FooMiddleware]
+    end
+  end
+
+  test "adding a middleware", %{test: id} do
+    mw = K8s.Middleware.RegistryTest.FooMiddleware
+    :ok = Registry.add(id, :request, mw)
+
+    middlewares = Agent.get(Registry, & &1[id])
+    assert middlewares == %{request: [K8s.Middleware.RegistryTest.FooMiddleware]}
+  end
+
+  test "setting the middleware stack", %{test: id} do
+    mw = K8s.Middleware.RegistryTest.FooMiddleware
+    :ok = Registry.add(id, :request, mw)
+
+    stack = [
+      K8s.Middleware.RegistryTest.BarMiddleware,
+      K8s.Middleware.RegistryTest.FooMiddleware
+    ]
+
+    :ok = Registry.set(id, :request, stack)
+
+    middlewares = Agent.get(Registry, & &1[id])
+
+    assert middlewares == %{
+             request: [
+               K8s.Middleware.RegistryTest.BarMiddleware,
+               K8s.Middleware.RegistryTest.FooMiddleware
+             ]
+           }
+  end
 end
