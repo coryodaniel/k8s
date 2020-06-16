5ea26e7b737d47d6b4aaef1a061ae05b426c2256
Praveen Perera <ppraveen25@gmail.com>
2020-06-16 16:09:08 -0400

    Remove implementation for RequestOptions

--- lib/k8s/conn/auth/digitalocean.ex
+++ lib/k8s/conn/auth/digitalocean.ex
@@ -18,16 +18,4 @@ defmodule K8s.Conn.Auth.DigitalOcean do
   end
 
   def create(_, _), do: nil
-
-  defimpl K8s.Conn.RequestOptions, for: __MODULE__ do
-    @doc "Generates HTTP Authorization options for certificate authentication"
-    @spec generate(K8s.Conn.Auth.Token.t()) :: K8s.Conn.RequestOptions.generate_t()
-    def generate(%K8s.Conn.Auth.Token{token: token}) do
-      {:ok,
-       %K8s.Conn.RequestOptions{
-         headers: [{"Authorization", "Bearer #{token}"}],
-         ssl_options: []
-       }}
-    end
-  end
 end
