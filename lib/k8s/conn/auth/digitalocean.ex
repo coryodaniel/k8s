e14eecba64ffe5b8c9f85e24fc7d1b7b9b015ad7
Praveen Perera <ppraveen25@gmail.com>
2020-06-16 16:12:15 -0400

    Only match on exec command intended for `doctl`

--- lib/k8s/conn/auth/digitalocean.ex
+++ lib/k8s/conn/auth/digitalocean.ex
@@ -7,8 +7,8 @@ defmodule K8s.Conn.Auth.DigitalOcean do
 
   @impl true
   @spec create(map() | any, String.t() | any) :: K8s.Conn.Auth.Token.t() | nil
-  def create(%{"exec" => %{"command" => command, "args" => args}}, _) do
-    with {json, 0} <- System.cmd(command, args),
+  def create(%{"exec" => %{"command" => "doctl", "args" => args}}, _) do
+    with {json, 0} <- System.cmd("doctl", args),
          {:ok, response_map} <- Jason.decode(json),
          token when not is_nil(token) <- get_in(response_map, ["status", "token"]) do
       %K8s.Conn.Auth.Token{token: token}
