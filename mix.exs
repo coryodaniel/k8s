575e854146c50ef50732d0d009d5574b4a73e095
Cory ODaniel <cory@coryodaniel.com>
2020-07-31 15:09:42 -0700

    0.5.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule K8s.MixProject do
     [
       app: :k8s,
       description: "Kubernetes API Client for Elixir",
-      version: "0.5.1",
+      version: "0.5.2",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
