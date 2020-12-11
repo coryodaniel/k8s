1d9a50f6328f8e62498a67550fd46d97507c8be7
Darren Clark <darren.clark@thescore.com>
2020-12-11 12:29:07 -0500

    Add test

--- test/k8s/client_test.exs
+++ test/k8s/client_test.exs
@@ -1,4 +1,40 @@
 defmodule K8s.ClientTest do
   use ExUnit.Case, async: true
   doctest K8s.Client
+
+  test "generateName with create/1" do
+    job = %{
+      "apiVersion" => "batch/v1",
+      "kind" => "Job",
+      "metadata" => %{
+        "namespace" => "default",
+        "generateName" => "hello-"
+      },
+      "spec" => %{
+        "template" => %{
+          "spec" => %{
+            "containers" => [
+              %{
+                "name" => "hello",
+                "image" => "busybox",
+                "args" => ["/bin/sh", "-c", "echo Hello, world"]
+              }
+            ],
+            "restartPolicy" => "OnFailure"
+          }
+        }
+      }
+    }
+
+    assert %K8s.Operation{
+             api_version: "batch/v1",
+             data: ^job,
+             label_selector: nil,
+             method: :post,
+             name: "Job",
+             path_params: [namespace: "default"],
+             query_params: %{},
+             verb: :create
+           } = K8s.Client.create(job)
+  end
 end
