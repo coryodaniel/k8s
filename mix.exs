ff4ff56d4fbc757f257b409aadd06474b32dc8e7
Michael Ruoss <michael.ruoss@ufirstgroup.com>
2022-02-20 14:29:54 +0100

    deps and dialyzer

--- mix.exs
+++ mix.exs
@@ -37,6 +37,8 @@ defmodule K8s.MixProject do
       {:jason, "~> 1.0"},
       {:telemetry, "~> 1.0"},
       {:opentelemetry_telemetry, "~> 1.0.0-beta.4", optional: true},
+      {:opentelemetry, "~> 1.0", optional: true},
+      {:spandex, "~> 3.0.3", optional: true},
 
       # dev/test deps (e.g. code coverage)
       {:inch_ex, github: "rrrene/inch_ex", only: [:dev, :test]},
@@ -94,7 +96,14 @@ defmodule K8s.MixProject do
   defp dialyzer do
     [
       ignore_warnings: ".dialyzer_ignore.exs",
-      plt_add_apps: [:mix, :eex],
+      plt_add_apps: [
+        :mix,
+        :eex,
+        :opentelemetry,
+        :opentelemetry_telemetry,
+        :opentelemetry_api,
+        :spandex
+      ],
       plt_core_path: "priv/plts",
       plt_file: {:no_warn, "priv/plts/k8s.plt"}
     ]
