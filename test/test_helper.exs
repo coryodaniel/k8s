9fee33880dd54ea19ef3f1fd59cf5aa5e9ff6c96
Michael Ruoss <michael.ruoss@ufirstgroup.com>
2022-09-09 22:53:24 +0200

    more reliable tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,3 @@
 K8s.Client.DynamicHTTPProvider.start_link(nil)
 K8s.Client.DynamicWebSocketProvider.start_link(nil)
-ExUnit.start(exclude: [:integration])
+ExUnit.start(exclude: [:integration, :reliability])
