10b956a9d839281a19150ab2e7b97d9241d72ef5
Michael Ruoss <michael@michaelruoss.ch>
2023-02-13 15:15:44 +0100

    auth module doc

--- lib/k8s/conn/auth.ex
+++ lib/k8s/conn/auth.ex
@@ -1,9 +1,28 @@
 defmodule K8s.Conn.Auth do
   @moduledoc """
   Authorization behaviour
-
-  `:skip` is used to signal to `K8s.Conn` to skip a provider that would not authenticate the current connection.
   """
 
-  @callback create(map(), String.t()) :: {:ok, struct} | {:error, any} | :skip
+  @doc """
+  Creates a struct holding the information to authenticate against the
+  API server.
+
+  ### Arguments
+
+  * `auth_map` - A map representing the `.users[].user` part from a kubeconfig.
+  * `base_path` - The path to the folder holding the kubeconfig file.
+
+  ### Result
+
+  The struct returned by this function MUST implement the
+  `K8s.Conn.RequestOptions` protocol.
+  Return `:skip` to signal to `K8s.Conn` to skip a provider that would not
+  authenticate the current connection.
+
+  ### Examples
+
+  See the various modules implementing this behaviour for examples.
+  """
+  @callback create(auth_map :: map(), base_path :: String.t()) ::
+              {:ok, struct} | {:error, any} | :skip
 end
