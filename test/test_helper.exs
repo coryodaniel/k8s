d3a0385426920e1ee13ec494d6707141bd7d84c4
Michael Ruoss <michael.ruoss@cradle.bio>
2025-05-29 17:56:02 +0200

    use action_elixir_run_tests for matrix

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,6 @@
 K8s.Client.DynamicHTTPProvider.start_link(nil)
-ExUnit.start(exclude: [:integration, :reliability])
+
+ExUnit.start(
+  exclude: [:integration, :reliability],
+  timeout: System.get_env("MIX_TEST_TIMEOUT", "60000") |> String.to_integer()
+)
